4bb35269344e5ca678952ee5979cf0f4001a90bf
Sandesh Sharma <Sandesh.Sharma@trojans.dsu.edu>
2020-11-05 20:22:49 -0600

    Add project files.

---
+++ P5/FakeIssueRepository.cs
@@ -0,0 +1,348 @@
+﻿using Castle.Core.Internal;
+using P5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace Builder
+{
+    public class FakeIssueRepository : IIssueRepository
+    {
+
+
+        public const string NO_ERROR = "";
+        public const string EMPTY_TITLE_ERROR = "A Title is required.";
+        public const string EMPTY_DISCOVERY_DATETIME_ERROR = "Must Select A Discovery Date/TIme.";
+        public const string FUTURE_DISCOVERY_DATETIME_ERROR = "Issues can't be from the future.";
+        public const string EMPTY_DISCOVERER_ERROR = "A Discoverer is required.";
+
+        private static List<Issue> _Issues = new List<Issue>();
+
+        public FakeIssueRepository()
+        {
+
+           
+            if (_Issues.Count == 0)
+            {
+                string added = Add(new Issue
+                {
+                    id = getNextIssueId(),
+                    project_id = 1,
+                    Title = "Hello there",
+                    DiscoveryDate = DateTime.Today,
+                    Discoverer = "Sharma, Sandesh",
+                    initialDescription = "This is my first one",
+                    Component = "This is some random stuff",
+                    IssueStatusid = 2
+
+                });
+                added = Add(new Issue
+                {
+                    id = getNextIssueId(),
+                    project_id = 1,
+                    Title = "How are you my friend",
+                    DiscoveryDate = DateTime.Today,
+                    Discoverer = "Sharma, Sandesh",
+                    initialDescription = "This is my first one",
+                    Component = "This is some random stuff",
+                    IssueStatusid = 2
+
+                });
+
+                added = Add(new Issue
+                {
+                    id = getNextIssueId(),
+                    project_id = 1,
+                    Title = "How are you",
+                    DiscoveryDate = DateTime.Today,
+                    Discoverer = "Bishop, Dave",
+                    initialDescription = "This is my first one",
+                    Component = "This is some random stuff",
+                    IssueStatusid = 2
+
+                });
+
+                if (string.IsNullOrEmpty(added) == false)
+                {
+                    MessageBox.Show(added, "Error!");
+                }
+            }
+
+      
+
+        }
+        public string Add(Issue issue)
+        {
+
+            string validation = this.ValidateIssue(issue);
+
+            if (string.IsNullOrEmpty(validation) == false)
+            {
+                return validation;
+            }
+            else
+            {
+                
+
+                issue.id = getNextIssueId();
+                _Issues.Add(issue);
+                
+                return NO_ERROR;
+            }
+
+        }
+        public List<Issue> GetAll(int ProjectId) { 
+
+            List<Issue> allIssues = new List<Issue>();
+            FormSelectProject myProject = new FormSelectProject();
+
+            int pId = myProject._SelectedProjectId;
+
+
+
+            foreach (Issue a in _Issues)
+            {
+                if (a.project_id == pId)
+                {
+                    allIssues.Append(a);
+
+                }
+            }
+            return allIssues;
+        }
+
+        public bool Remove(Issue issue) {
+
+            int index = 0;
+            bool success = false;
+            foreach(Issue iss  in _Issues)
+            {
+                if(iss.id == issue.id)
+                {
+                    _Issues.RemoveAt(index);
+                    success = true;
+                }
+
+                index++;
+            }
+
+            return success;
+            
+
+        }
+
+        public string Modify(Issue issue) {
+
+            string validation = this.ValidateIssue(issue);
+         
+            if(string.IsNullOrEmpty(validation) == false)
+            {
+                return validation;
+            }
+            else
+            {
+                int index = 0;
+                foreach (Issue iss in _Issues)
+                {
+                    if (iss.project_id == issue.project_id)
+                    {
+
+                        _Issues[index] = issue;
+                    }
+
+                    index++;
+                }
+
+                return NO_ERROR;
+            }
+        
+        }
+
+        public int GetTotalNumberOfIssues(int ProjectId)
+        {
+            int a = 0;
+            foreach(Issue iss in _Issues)
+            {
+                if(iss.project_id == ProjectId)
+                {
+                    a++;
+                }
+            }
+            return a;
+
+
+        }
+
+        public List<string> GetIssuesByMonth(int ProjectId)
+        {
+
+            List<string> monthlyIssues = new List<string>();
+            string key;
+            Dictionary<string, int> mnIssues = new Dictionary<String, int>();
+
+            foreach(Issue iss in _Issues)
+            {
+                if(iss.project_id == ProjectId)
+                {
+                    key = iss.DiscoveryDate.Year.ToString() + " - " + iss.DiscoveryDate.Month.ToString();
+                    if (mnIssues.ContainsKey(key))
+                    {
+
+                        mnIssues[key]++;
+                        
+                    }
+                    else
+                    {
+                        mnIssues.Add(key, 1);
+                    }
+                    
+                }
+
+            }
+
+            foreach(KeyValuePair<string, int> mnIss in mnIssues)
+            {
+                key = (mnIss.Key + " : " + mnIss.Value.ToString()+'\n');
+                monthlyIssues.Add(key);
+            }
+
+            return monthlyIssues;
+        }
+
+        public List<string> GetIssuesByDiscoverer(int ProjectId)
+        {
+            List<string> mylist = new List<string>();
+            Dictionary<string, int> issByDisc = new Dictionary<string, int>();
+            
+
+            foreach(Issue iss in _Issues)
+            {
+
+                if(iss.project_id == ProjectId)
+                {
+
+                    if (issByDisc.ContainsKey(iss.Discoverer))
+                    {
+                        issByDisc[iss.Discoverer]++;
+                    }
+                    else
+                    {
+                        issByDisc.Add(iss.Discoverer,1);
+                    }
+                }
+            }
+
+            string key;
+
+            foreach(KeyValuePair<string,int> dict in issByDisc)
+            {
+                key = dict.Key + " : " + dict.Value.ToString()+'\n';
+                mylist.Add(key);
+            }
+
+
+            return mylist;
+
+        }
+
+        public Issue GetIssueById(int Id)
+        {
+            Issue myIssue = new Issue();
+            foreach(Issue a in _Issues)
+            {
+                if (a.id == Id){
+
+                    myIssue = a;
+                   
+                }
+            }
+
+            return myIssue;
+
+        }
+
+
+        private string ValidateIssue(Issue issue)
+        {
+
+            string datetime = issue.DiscoveryDate.ToString();
+
+            if (issue.Title.Trim() == string.Empty)
+            {
+                return EMPTY_TITLE_ERROR;
+
+            }
+            else if (issue.DiscoveryDate > DateTime.Now)
+            {
+
+                return FUTURE_DISCOVERY_DATETIME_ERROR;
+
+            }
+            else if (issue.Discoverer == string.Empty)
+            {
+
+                return EMPTY_DISCOVERER_ERROR;
+
+            }
+            else if (string.IsNullOrEmpty(datetime))
+            {
+
+                return EMPTY_DISCOVERY_DATETIME_ERROR;
+            }
+            else
+            {
+                bool res = IsDupllicate(issue.Title.Trim());
+
+                
+                
+                if (res == true){
+
+                    return "Title Already Exists.";
+                }
+                else {
+                    return NO_ERROR;
+                }
+
+                
+            }
+        }
+
+
+        private bool IsDupllicate(string title)
+        {
+            bool result = false;
+         
+            foreach (Issue iss in _Issues)
+            {
+
+                if(iss.Title == title)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public int getNextIssueId()
+        {
+
+            int biggest = 0;
+            foreach(Issue iss in _Issues)
+            {
+                if(iss.id > biggest)
+                {
+                    biggest = iss.id;
+                    
+                }
+            }
+
+            return biggest + 1;
+               
+        }
+    }
+}
---
+++ P5/Select Issue.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Builder
+{
+    partial class SelectIssue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.selIss = new System.Windows.Forms.Button();
+            this.cnclSel = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tille = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.discDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.disc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.initDesc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.tille,
+            this.discDate,
+            this.disc,
+            this.initDesc,
+            this.comp,
+            this.stat});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(927, 355);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // selIss
+            // 
+            this.selIss.Location = new System.Drawing.Point(786, 440);
+            this.selIss.Name = "selIss";
+            this.selIss.Size = new System.Drawing.Size(153, 38);
+            this.selIss.TabIndex = 1;
+            this.selIss.Text = "Select Issue";
+            this.selIss.UseVisualStyleBackColor = true;
+            // 
+            // cnclSel
+            // 
+            this.cnclSel.Location = new System.Drawing.Point(656, 440);
+            this.cnclSel.Name = "cnclSel";
+            this.cnclSel.Size = new System.Drawing.Size(99, 38);
+            this.cnclSel.TabIndex = 2;
+            this.cnclSel.Text = "Cancel";
+            this.cnclSel.UseVisualStyleBackColor = true;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.Width = 40;
+            // 
+            // tille
+            // 
+            this.tille.HeaderText = "Title";
+            this.tille.MinimumWidth = 6;
+            this.tille.Name = "tille";
+            this.tille.Width = 180;
+            // 
+            // discDate
+            // 
+            this.discDate.HeaderText = "Discovery Date";
+            this.discDate.MinimumWidth = 6;
+            this.discDate.Name = "discDate";
+            this.discDate.Width = 125;
+            // 
+            // disc
+            // 
+            this.disc.HeaderText = "Discoverer";
+            this.disc.MinimumWidth = 6;
+            this.disc.Name = "disc";
+            this.disc.Width = 125;
+            // 
+            // initDesc
+            // 
+            this.initDesc.HeaderText = "Initial Description";
+            this.initDesc.MinimumWidth = 6;
+            this.initDesc.Name = "initDesc";
+            this.initDesc.Width = 125;
+            // 
+            // comp
+            // 
+            this.comp.HeaderText = "Component";
+            this.comp.MinimumWidth = 6;
+            this.comp.Name = "comp";
+            this.comp.Width = 125;
+            // 
+            // stat
+            // 
+            this.stat.HeaderText = "Status";
+            this.stat.MinimumWidth = 6;
+            this.stat.Name = "stat";
+            this.stat.Width = 125;
+            // 
+            // SelectIssue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 506);
+            this.Controls.Add(this.cnclSel);
+            this.Controls.Add(this.selIss);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SelectIssue";
+            this.Text = "SelectIssue";
+            this.Load += new System.EventHandler(this.SelectIssue_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button selIss;
+        private System.Windows.Forms.Button cnclSel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tille;
+        private System.Windows.Forms.DataGridViewTextBoxColumn discDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn disc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn initDesc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn comp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stat;
+    }
+}
